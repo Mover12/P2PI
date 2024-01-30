1ef1e1daa2d7a24601e94f2b71ffedfa4447ed7c
Mover12 <mixail.eli.2416@gmail.com>
2024-01-30 22:11:13 +0300

    WebSocket

--- src/index.js
+++ src/index.js
@@ -1,37 +1,80 @@
 const express = require('express');
-const fs = require('fs')
-const path = require('path')
+const ws = require('ws');
+const path = require('path');
+const uuid = require('uuid');
+
+
+const PORT = 8000;
+const WSPORT = 5000;
+const IP = 'localhost';
 
 const app = express();
-const PORT = 8000;
-const IP = '127.0.0.1';
+const wss = new ws.Server({
+    port: WSPORT,
+    url: IP
+});
+
+var clients = {};
+var hosts = {};
+var positions = {};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', express.static(path.resolve('public')));
 app.use('/src', express.static(path.resolve('src')));
-app.post('/download', (req, res) => {
-    var pos = req.body['pos'];
-    fs.readFile(path.resolve('data', `${pos}`), (err, data) => {
-        if(!err && data) {
-            res.send({ [pos] : JSON.parse(String(data)) });
-        } else {
-            res.send({ 'ERROR' : err});
+
+
+wss.on('connection', ws => {
+    ws.id = uuid.v4();
+    clients[ws.id] = ws;
+    positions[ws.id] = [];
+    ws.on('close', () => {
+        positions[ws.id].forEach(pos => {
+            hosts[pos].splice(hosts[pos].indexOf(ws.id), 1);
+            if (hosts[pos].length == 0) {
+                delete hosts[pos]
+            }
+        });
+        delete positions[ws.id];
+        delete clients[ws.id]
+    });
+    ws.on('message', message => {
+        var data = JSON.parse(message);
+        if (data.event == 'get') {
+            if (hosts[data.pos]) {
+                hosts[data.pos].forEach(host => {
+                    if (host != ws.id) {
+                        clients[host].send(JSON.stringify({
+                            event: 'get',
+                            pos: data.pos
+                        }));
+                        return true;
+                    }
+                });
+            } else {
+                hosts[data.pos] = [];
+            }
+            hosts[data.pos].push(ws.id);
+            positions[ws.id].push(data.pos);
+        } else if (data.event == 'put') {
+            hosts[data.pos].forEach(listener => {
+                if (listener != ws.id) {
+                    clients[listener].send(JSON.stringify({
+                        event: 'put',
+                        pos: data.pos,
+                        chunk: data.chunk
+                    }));
+                }
+            });
+        } else if (data.event == 'pop') {
+            positions[ws.id].splice(positions[ws.id].indexOf(data.pos), 1);
+            hosts[data.pos].splice(hosts[data.pos].indexOf(ws.id), 1);
+            if (hosts[data.pos].length == 0) {
+                delete hosts[data.pos]
+            }
         }
     })
-})
-app.post('/upload', (req, res) => {
-    var data = req.body;
-    for (const pos in data) {
-        fs.writeFile(path.resolve('data', `${pos}`), String(JSON.stringify(data[pos])), (err) => {
-            if(!err) {
-                res.send(err);
-            } else {
-                res.send({ 'ERROR' : err});
-            }
-        })
-    }
-})
+});
 
 app.listen(PORT, IP);
--- src/main.js
+++ src/main.js
@@ -1,25 +1,30 @@
 import Vector from './class/Vector.js'
 import Chunk from './class/Chunk.js'
+import Converter from './class/Converter.js'
 
-
-var cnv = document.getElementById("cnv");
+const socket = new WebSocket('ws://localhost:5000');
+var cnv = document.querySelector(".canvas");
+var position = document.querySelector(".position");
 var cnv2d = cnv.getContext("2d");
 
 var chunks = new Map();
 var chunksQueue = new Array();
+var colors = ['#FFFFFF', '#FF4040'];
 
 var camera = new Vector(0, 0);
 var prevCamera = new Vector(0, 0);
 var scale = new Vector(1, 1);
 
+const width = cnv2d.canvas.width;
+const height = cnv2d.canvas.height;
 const cellCount = 64;
 const cellPSize = 10;
 const chunkPSize = cellCount * cellPSize;
 
 const renderDistance = 1;
-const chunksCashSize= 128;
+const chunksCashSize = 16;
 
-const zoomSpeed = 0.1;
+const zoomSpeed = 1;
 const zoomMaximum = 10;
 
 
@@ -27,13 +32,20 @@ function generateChunk(pos) {
     var chunk = chunks.get(pos.stringify());
     
     if (chunk == null) {
-        chunk = downloadChunk(pos.stringify());
+        chunk = new Chunk(cellCount);
+        socket.send(JSON.stringify({
+            event: 'get',
+            pos: pos.stringify()
+        }));
         chunksQueue.push(pos);
         chunks.set(pos.stringify(), chunk);
         if (chunksQueue.length > chunksCashSize) {
-            let pos = chunksQueue.shift();
-            uploadChunk(pos, chunks.get(pos.stringify()));
-            chunks.delete(pos.stringify());
+            let pos = chunksQueue.shift().stringify();
+            socket.send(JSON.stringify({
+                event: 'pop',
+                pos: pos
+            }));
+            chunks.delete(pos);
       }
     }
 }
@@ -45,51 +57,19 @@ function generateNearChunks(pos) {
       }
     }
   }
-
-function uploadChunk(pos, chunk) {
-    fetch("/upload", {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ [pos] : chunk })
-    });
-}
-  
-function downloadChunk(pos) {
-    var chunk = new Chunk(cellCount);
-    fetch("/download", {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ pos })
-    }).then(function(res) {
-        return res.json()
-    }).then(function(data) {
-        if (Object.keys(data)[0] != 'ERROR') {
-            chunk.cellCount = data[Object.keys(data)]['cellCount']
-            chunk.cells = data[Object.keys(data)]['cells']
-        }
-    });
-    return chunk;
-}
   
 function renderChunks(chunksQueue) {
     chunksQueue.forEach(chunkPos => {
         for (let i = 0; i < cellCount; i++) {
             for (let j = 0; j < cellCount; j++) {
-                var cell = chunks.get(chunkPos.stringify()).cells[i][j];
-                var cellColor = '#000000';
-                if (cell == 0) {
-                    cellColor = '#FFFFFF';
-                }
-                else if (cell == 1) {
-                    cellColor = '#FF4040';
-                }
-                var cellPos = convertC2Mo(new Vector(
+                var cellColor = chunks.get(chunkPos.stringify()).cells[i][j];
+                var cellPos = Converter.CaToMo(new Vector(
                     j * cellPSize + chunkPos.x * chunkPSize, 
                     i * cellPSize + chunkPos.y * chunkPSize
-                ));
-                if ((-cellPSize * scale.x <= cellPos.x && cellPos.x <= cnv2d.canvas.width) && (-cellPSize * scale.y <= cellPos.y && cellPos.y <= cnv2d.canvas.height)) {
-                    cnv2d.fillStyle = cellColor;
-                    cnv2d.fillRect(cellPos.x, cellPos.y, cellPSize * scale.x, cellPSize * scale.y);
+                ), camera, scale);
+                if ((-cellPSize * scale.x <= cellPos.x && cellPos.x <= width) && (-cellPSize * scale.y <= cellPos.y && cellPos.y <= height)) {
+                    cnv2d.fillStyle = colors[cellColor];
+                    cnv2d.fillRect(Math.trunc(cellPos.x), Math.trunc(cellPos.y), Math.trunc(cellPSize * scale.x), Math.trunc(cellPSize * scale.y));
                 }
             }
         }
@@ -100,11 +80,11 @@ function mousePress(event) {
     if (event.buttons == 4) {
         prevCamera.set(event.offsetX, event.offsetY);
     }
-    document.addEventListener('mousemove', mouseMove)
+    cnv.addEventListener('mousemove', mouseMove)
 }
 
 function mouseRelease() {
-    document.removeEventListener('mousemove', mouseMove);
+    cnv.removeEventListener('mousemove', mouseMove);
 }
 
 function mouseMove(event) {
@@ -113,72 +93,55 @@ function mouseMove(event) {
         camera.y = camera.y - (event.offsetY - prevCamera.y) / scale.y;
         prevCamera.set(event.offsetX, event.offsetY);
     } else {
-        var chunkPos = convertM2Ch(convertM2Ca(new Vector(event.offsetX, event.offsetY)));
-        var cellPos = convertM2Ce(convertM2Ca(new Vector(event.offsetX, event.offsetY)));
+        const chunkPos = Converter.CaToCh(Converter.MoToCa(new Vector(event.offsetX, event.offsetY), camera, scale), chunkPSize);
+        const cellPos = Converter.CaToCe(Converter.MoToCa(new Vector(event.offsetX, event.offsetY), camera, scale), chunkPSize, cellPSize);
         const chunk = chunks.get(chunkPos.stringify());
         if (chunk != null) {
             if (event.buttons == 1) {
                 chunk.cells[cellPos.y][cellPos.x] = 1;
             }
-            else if (event.buttons == 2) {
-                chunk.cells[cellPos.y][cellPos.x] = 0;
-            }
+            socket.send(JSON.stringify({
+                event: 'put',
+                pos: chunkPos.stringify(),
+                chunk: chunk
+            }));
         }
-    }
-}
-
-function keyPress(event) {
-    console.log(event);
-    if (event.code == 'KeyS') {
-        chunks.keys().forEach((chunkPos) => {
-            uploadChunk(chunkPos, chunks.get(chunkPos));
-        })
     }
 }
 
 function wheel(event) {
-    var cameraBefore = convertM2Ca(new Vector(event.offsetX, event.offsetY));
+    var cameraBeforeZoom = Converter.MoToCa(new Vector(event.offsetX, event.offsetY), camera, scale);
     if (event.deltaY < 0) {
-        if (Math.floor(scale.x) < zoomMaximum && Math.floor(scale.y) < zoomMaximum) {
+        if (scale.x < zoomMaximum && scale.y < zoomMaximum) {
             scale = scale.add(new Vector(zoomSpeed, zoomSpeed));
         }
     } else {
-        if (Math.floor(scale.x) > zoomSpeed && Math.floor(scale.y) > zoomSpeed) {
+        if (scale.x > zoomSpeed && scale.y > zoomSpeed) {
             scale = scale.sub(new Vector(zoomSpeed, zoomSpeed));
         }
     }
-    var cameraAfter = convertM2Ca(new Vector(event.offsetX, event.offsetY));
-    camera = camera.add(cameraBefore.sub(cameraAfter));
+    var cameraAfterZoom = Converter.MoToCa(new Vector(event.offsetX, event.offsetY), camera, scale);
+    camera = camera.add(cameraBeforeZoom.sub(cameraAfterZoom));
 }
 
-
-function convertC2Mo(pos) {
-    return pos.sub(camera).mult(scale);
-}
-
-function convertM2Ca(pos) {
-    var cameraX = camera.x + pos.x / scale.x;
-    var cameraY = camera.y + pos.y / scale.y;
-    return new Vector(cameraX, cameraY);
-}
-
-function convertM2Ch(pos) {
-    var chunkX = Math.floor(pos.x / (chunkPSize));
-    var chunkY = Math.floor(pos.y / (chunkPSize));
-    return new Vector(chunkX, chunkY);
-}
-
-function convertM2Ce(pos) {
-    var cellX = Math.floor((pos.x - convertM2Ch(pos).x * chunkPSize) / (cellPSize));
-    var cellY = Math.floor((pos.y - convertM2Ch(pos).y * chunkPSize) / (cellPSize));
-    return new Vector(cellX, cellY);
-}
-
+socket.onmessage = message => {
+    var data = JSON.parse(message.data);
+    if (data.event == 'get') {
+        socket.send(JSON.stringify({
+            event: 'put',
+            pos: data.pos,
+            chunk: chunks.get(data.pos)
+        }));
+    } else if (data.event == 'put') {
+        chunks.set(data.pos, data.chunk);
+    }
+};
 
 setInterval(() => {
-    cnv2d.fillStyle = "black"
-    cnv2d.fillRect(0, 0, cnv2d.canvas.width, cnv2d.canvas.height)
-    generateNearChunks(convertM2Ch(new Vector(camera.x + cnv2d.canvas.width / 2, camera.y + cnv2d.canvas.height / 2)));
+    position.innerHTML = `${Math.floor(camera.x)}, ${Math.floor(camera.y)}`;
+    cnv2d.fillStyle = "white";
+    cnv2d.fillRect(0, 0, width, height);
+    generateNearChunks(Converter.CaToCh(new Vector(camera.x, camera.y), chunkPSize));
     renderChunks(chunksQueue);
 }, 1);
 
@@ -186,5 +149,3 @@ setInterval(() => {
 cnv.addEventListener('mousedown', mousePress);
 cnv.addEventListener('mouseup', mouseRelease);
 cnv.addEventListener('wheel', wheel);
-
-document.addEventListener('keydown', keyPress);
